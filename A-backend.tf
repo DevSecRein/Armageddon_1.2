6113fb6dbcdbc7d9ae7b8081ec2af0202d99824c
DevSecRein <rilesyen@gmail.com>
2025-01-06 18:29:36 -0600

    Update A-backend.tf

--- A-backend.tf
+++ A-backend.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3" {
-        bucket = "walid-backend-089.com"
+        bucket = "staticrein.com"
         key = "terraformv2.tfstate" #name of the state file that
         region = "us-east-1"  
     }
